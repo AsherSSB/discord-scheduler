86c24ede39b55fcc3be5147b4056a60caec3bae3
AsherSSB <gromanenghi04@gmail.com>
2025-10-16 21:45:40 -0700

    adds server-client auth scaffold

---
+++ src/client/client.js
@@ -0,0 +1,67 @@
+import { DiscordSDK } from "https://cdn.skypack.dev/@discord/embedded-app-sdk";
+
+
+// Will eventually store the authenticated user's access_token
+let auth;
+
+let discordSdk; 
+
+getClientId().then((client_id) => {
+  console.debug(client_id)
+  discordSdk = new DiscordSDK(client_id);
+  setupDiscordSdk(client_id).then(() => {
+    console.log("Discord SDK is authenticated");
+  })
+});
+
+async function getClientId() {
+  const response = await fetch("api/client-id");
+  const { client_id } = await response.json();
+  return client_id;
+}
+
+async function setupDiscordSdk(clientId) {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  // Authorize with Discord Client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: clientId,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+      "applications.commands"
+    ],
+  });
+
+  // Retrieve an access_token from your activity's server
+  const response = await fetch("/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+}
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello, World!</h1>
+  </div>
+`;
+
---
+++ src/server/server.py
@@ -0,0 +1,62 @@
+import uvicorn
+import aiohttp
+import os
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+from typing import cast
+from pathlib import Path
+
+DISCORD_TOKEN_URL = "https://discord.com/api/oauth2/token"
+
+_ = load_dotenv()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with aiohttp.ClientSession() as session:
+        app.state.aiohttp_session = session
+        yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/nuts")
+async def test():
+    return {"message": "nuts"}
+
+
+@app.get("/api/client-id")
+async def send_client_id():
+    return {
+        "client_id": os.environ["DISCORD_CLIENT_ID"],
+    }
+
+
+@app.post("/api/authenticate")
+async def authenticate_client(auth_code: str):
+    payload = {
+        "client_id": os.environ["DISCORD_CLIENT_ID"],
+        "client_secret": os.environ["DISCORD_CLIENT_SECRET"],
+        "grant_type": "authorization_code",
+        "code": auth_code,
+    }
+
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            DISCORD_TOKEN_URL, data=payload, headers=headers
+        ) as resp:
+            response_data = cast(dict[str, str], await resp.json())
+            return response_data
+
+
+app.mount("/", StaticFiles(directory=Path("../client/"), html=True), name="static")
+
+if __name__ == "__main__":
+    # set reload=False for production
+    uvicorn.run(app, host="127.0.0.1", port=8001, reload=True)
