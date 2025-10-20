3574d53546729c42cc34c65ede4ccca951e5a1fc
AsherSSB <gromanenghi04@gmail.com>
2025-10-19 20:22:39 -0700

    adds server endpoints for dabase connections

--- src/server/server.py
+++ src/server/server.py
@@ -1,17 +1,24 @@
+from uuid import UUID
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 import uvicorn
 import aiohttp
 import os
 
-from fastapi import FastAPI
+from datetime import time, date
+from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 from typing import cast
 from pathlib import Path
 
+from utils.database import Database
+
 DISCORD_TOKEN_URL = "https://discord.com/api/oauth2/token"
 
 _ = load_dotenv()
+
+db = Database()
 
 
 @asynccontextmanager
@@ -22,11 +29,6 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
-
-@app.get("/nuts")
-async def test():
-    return {"message": "nuts"}
 
 
 @app.get("/api/client-id")
@@ -54,6 +56,37 @@ async def authenticate_client(code_payload: dict[str, str]):
         ) as resp:
             response_data = cast(dict[str, str], await resp.json())
             return response_data
+
+
+@app.post("/api/user")
+async def create_user(user_id: int):
+    db.create_user(user_id)
+    return {"message": "successfully created user"}
+
+
+@app.get("/api/poll/{poll_id}")
+async def get_poll(poll_id: UUID):
+    if not (poll := db.get_poll(poll_id)):
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+    return poll.model_dump()
+
+
+@app.post("/api/poll")
+async def create_poll(
+    name: str,
+    creator_id: int,
+    start_time: time,
+    end_time: time,
+    start_date: date,
+    end_date: date,
+):
+    poll_id = db.create_poll(
+        name, creator_id, start_time, end_time, start_date, end_date
+    )
+    if not poll_id:
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return {"id": poll_id}
 
 
 app.mount("/", StaticFiles(directory=Path("../client/dist"), html=True), name="static")
